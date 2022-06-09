6ff98c122fb7248b2a5b06fbeffb22db6946a81a
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-06-09 14:09:24 +0800

    add sample shipping data

---
+++ _docs/04-oauth-google.md
@@ -0,0 +1,14 @@
+# Oauth using Google
+
+## Endpoint
+
+Generate a URL to request access from Google's OAuth 2.0 endpoint at:
+https://accounts.google.com/o/oauth2/v2/auth. 
+
+
+This endpoint is accessible over HTTPS; plain HTTP connections are refused.
+
+
+# Reference
+
+https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
--- basic-angular-app/src/app/app.module.ts
+++ basic-angular-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { SamplePage2Component } from './sample-page2/sample-page2.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
